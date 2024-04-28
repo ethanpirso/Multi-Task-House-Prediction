6cf50578f4e61d0e22b3a316064cab6818f0ef4c
Ethan Pirso <56749278+ethanpirso@users.noreply.github.com>
2024-04-28 19:01:14 -0400

    Defined seperate optimizers to address scale differences

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 The goal of this project is to build a multi-task learning model using PyTorch Lightning that predicts both house prices (a regression task) and house categories (a classification task). This involves using a shared bottom neural network architecture with task-specific top layers, applying advanced machine learning techniques and model management features provided by PyTorch Lightning.
 
 ## Dataset
-The project utilizes the "House Prices - Advanced Regression Techniques" dataset. A new variable 'House Category' is created from features such as 'House Style', 'Bldg Type', 'Year Built', and 'Year Remod/Add', to classify houses into distinct categories alongside predicting their prices.
+The project utilizes the "House Prices - Advanced Regression Techniques" dataset from kaggle, which can be downloaded here https://www.kaggle.com/c/house-prices-advanced-regression-techniques. A new variable 'House Category' is created from features such as 'House Style', 'Bldg Type', 'Year Built', and 'Year Remod/Add', to classify houses into distinct categories alongside predicting their prices.
 
 ## Installation
 
@@ -48,7 +48,7 @@ python evaluation/evaluate_model.py
 
 ## Project Structure
 - `data/`: Contains raw and processed data.
-- `models/`: Includes the multi-task learning model definition.
+- `models/`: Includes the multi-task learning model definition and .pkl file.
 - `preprocessing/`: Scripts for data cleaning and preparation.
 - `training/`: Training and validation logic.
 - `evaluation/`: Model evaluation scripts.
@@ -70,6 +70,16 @@ Hyperparameter optimization is performed using PyTorch Lightning's integration w
 ```bash
 python training/tune_hyperparameters.py
 ```
+
+## Results
+
+After training and evaluating our multi-task learning model, we achieved promising results on our test dataset. The model's performance is summarized as follows:
+
+- **Accuracy:** The model achieved an accuracy of approximately 83.90% in categorizing houses into one of the 8 categories. This high level of accuracy indicates that the model is quite effective in understanding and classifying the complex patterns associated with the diverse house categories.
+
+- **RMSE:** The Root Mean Square Error (RMSE) for the house price prediction task was around 60,196.13. While there's room for improvement, this value suggests that the model has a decent grasp on predicting house prices, considering the variability and range of house prices in the dataset.
+
+These results demonstrate the capability of our multi-task learning approach in handling the intricacies of predicting multiple outputs simultaneously. Future work will focus on further optimizing the model to improve both accuracy and RMSE, potentially through more advanced feature engineering, model architecture adjustments, and hyperparameter tuning.
 
 ## View Logged Runs in the MLFlow UI
 
--- models/multi_task_model.py
+++ models/multi_task_model.py
@@ -7,18 +7,16 @@ class MultiTaskModel(pl.LightningModule):
     def __init__(self, class_weights=None):
         super().__init__()
         self.class_weights = class_weights
+        self.automatic_optimization = False
         # Enhanced architecture with a more complex and deeper shared layer structure for improved feature extraction and performance
         self.shared_layers = nn.Sequential(
-            nn.Linear(63, 256),  # Reduced initial expansion for capturing essential relationships
-            nn.BatchNorm1d(256),
+            nn.Linear(63, 128),  # Reduced initial expansion for capturing essential relationships
+            nn.BatchNorm1d(128),
             nn.ReLU(),  # Using ReLU for initial layer for better gradient flow
             nn.Dropout(0.1),
-            nn.Linear(256, 512),
-            nn.BatchNorm1d(512),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
             nn.GELU(),  # Mixing GELU and ReLU for diverse activation patterns
-            nn.Dropout(0.1),
-            nn.Linear(512, 256),  # Reducing dimensionality sooner for attention mechanism compatibility
-            nn.ReLU(),  # Back to ReLU for a mix of activation functions
             nn.Dropout(0.1),
             nn.Linear(256, 256),  # Maintaining dimensionality for consistent feature representation
             nn.GELU(),  # Using GELU for deeper layers
@@ -52,21 +50,21 @@ class MultiTaskModel(pl.LightningModule):
         self.classification_head = nn.Sequential(
             nn.Linear(256, 512),  # Slightly expanded layer for feature capture
             nn.BatchNorm1d(512),
-            nn.LeakyReLU(negative_slope=0.02),  # Utilizing LeakyReLU for non-linear activation with a small slope for negative inputs
-            nn.Dropout(0.3),  # Maintaining dropout for regularization
+            nn.LeakyReLU(negative_slope=0.1),  # Using LeakyReLU for better gradient flow
+            nn.Dropout(0.2), 
             nn.Linear(512, 1024),
             nn.BatchNorm1d(1024),
-            nn.ReLU(),  # Keeping ReLU here for activation
+            nn.LeakyReLU(negative_slope=0.1),
             nn.Dropout(0.2),
-            nn.Linear(1024, 512),  # Reducing dimensionality while preserving essential information
+            nn.Linear(1024, 512),  
             nn.LayerNorm(512),
-            nn.LeakyReLU(negative_slope=0.01),  # Using LeakyReLU again for a slight negative slope activation
-            nn.Dropout(0.2),
-            nn.Linear(512, 256),  # Final refinement step before classification output
+            nn.GELU(),  
+            nn.Dropout(0.15),
+            nn.Linear(512, 256),  
             nn.LayerNorm(256),
-            nn.ReLU(),  # Back to ReLU for the final activation before output
-            nn.Dropout(0.1),
-            nn.Linear(256, 8)  # Output layer for 8 classes, matching the task requirement
+            nn.GELU(),  
+            nn.Dropout(0.15),
+            nn.Linear(256, 8) 
         )
 
     def forward(self, x):
@@ -86,11 +84,31 @@ class MultiTaskModel(pl.LightningModule):
         price_pred, category_pred = self(x)
         loss_fn_reg = torch.nn.MSELoss(reduction='sum')
         loss_fn_class = torch.nn.CrossEntropyLoss(weight=self.class_weights)  # Added class weights for better class balancing
-        loss_price = torch.sqrt(loss_fn_reg(price_pred.squeeze(), y_price) / len(y_price))  # RMSE for regression
+        
+        # Compute RMSE for regression
+        loss_price = torch.sqrt(loss_fn_reg(price_pred.squeeze(), y_price) / len(y_price))
+        self.log('train_rmse', loss_price, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        # Compute accuracy for the categorical
+        _, preds = torch.max(category_pred, dim=1)
+        correct_count = (preds == y_category).sum().item()
+        total_count = y_category.size(0)
+        accuracy = correct_count / total_count
+        self.log('train_accuracy', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
         loss_category = loss_fn_class(category_pred, y_category)
-        loss = loss_price + loss_category
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return loss
+        scaling_factor = 0.000001  # Added scaling factor for balancing the two losses
+        total_loss = (scaling_factor * loss_price) + loss_category
+        self.log('train_loss', total_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        # Manually backpropagate loss for each optimizer
+        for optimizer in self.optimizers():
+            optimizer.zero_grad()
+        total_loss.backward()
+        for optimizer in self.optimizers():
+            optimizer.step()
+        
+        return total_loss
     
     def test_step(self, batch, batch_idx):
         x, y_price, y_category = batch
@@ -110,8 +128,21 @@ class MultiTaskModel(pl.LightningModule):
         self.log('test_accuracy', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         return {'test_rmse': loss_price, 'test_accuracy': accuracy}
-
+    
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.005)  # Adjusted learning rate and optimizer for better convergence
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0.001)  # Adjusted scheduler for smoother learning rate changes
-        return [optimizer], [scheduler]
+        # Shared layers optimizer
+        optimizer_shared = torch.optim.Adam(self.shared_layers.parameters(), lr=0.005)
+        # Regression head optimizer
+        optimizer_regression = torch.optim.Adam(self.regression_head.parameters(), lr=0.0035)
+        # Classification head optimizer
+        optimizer_classification = torch.optim.Adam(self.classification_head.parameters(), lr=0.01)
+
+        # Define learning rate schedulers for each optimizer if necessary
+        scheduler_shared = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_shared, T_max=10, eta_min=0.0001)
+        scheduler_regression = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_regression, T_max=10, eta_min=0.0001)
+        scheduler_classification = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_classification, T_max=10, eta_min=0.002)
+
+        optimizers = [optimizer_shared, optimizer_regression, optimizer_classification]
+        schedulers = [scheduler_shared, scheduler_regression, scheduler_classification]
+
+        return optimizers, schedulers
